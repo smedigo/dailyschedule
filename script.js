c5e96e7795b8849ea7a106825e4976969a24b479
smedigo <smedigo@gmail.com>
2020-11-07 19:05:29 -0800

    newfile

--- script.js
+++ script.js
@@ -1,18 +1,109 @@
 // Get the current hour of the day using moment.js
-var currentMoment = moment();
-var data = currentMoment. format();
 
 // Option 1
-var currentHourOne= parseInt(moment().format("H"));
+var currentDateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+var currentDay = moment().format("D");
+var currentHour = moment().format("H");
+console.log(currentHour)
+console.log(currentDay)
+console.log(currentDateTime)
 
-// Option 2
-var currentHourTwo = currentMoment.hour();
+$("#currentDay").text(currentDateTime)
 
 //FOR Loop over the hours of the day from i=9 to i<== 17
 
-for (var i=9; i<; i++) {
-    
-}
+// for (var i=9; i<17; i++) {
+// }
+        if (9 < currentHour){
+            $("#9a").addClass("past")
+        }
+        else if (9 == currentHour){
+            $("#9a").addClass("present")
+        }
+        else if (9 > currentHour){
+            $("#9a").addClass("future")
+        }
+
+        // if statements for row 10am
+        if (10 < currentHour){
+            $("#10a").addClass("past")
+        }
+        else if (10 == currentHour){
+            $("#10a").addClass("present")
+        }
+        else if (10 > currentHour){
+            $("#10a").addClass("future")
+        }
+        // if statements for row 11am
+        if (11 < currentHour){
+            $("#11a").addClass("past")
+        }
+        else if (11 == currentHour){
+            $("#11a").addClass("present")
+        }
+        else if (11 > currentHour){
+            $("#11a").addClass("future")
+        }
+        // if statements for row 12pm
+        if (12 < currentHour){
+            $("#12p").addClass("past")
+        }
+        else if (12 == currentHour){
+            $("#12p").addClass("present")
+        }
+        else if (12 > currentHour){
+            $("#12p").addClass("future")
+        }
+
+        if (1 < currentHour){
+            $("#1p").addClass("past")
+        }
+        else if (1 == currentHour){
+            $("#1p").addClass("present")
+        }
+        else if (12 > currentHour){
+            $("#1p").addClass("future")
+        }
+
+        if (2 < currentHour){
+            $("#2p").addClass("past")
+        }
+        else if (2 == currentHour){
+            $("#2p").addClass("present")
+        }
+        else if (2 > currentHour){
+            $("#12p").addClass("future")
+        }
+
+        if (3 < currentHour){
+            $("#3p").addClass("past")
+        }
+        else if (3 == currentHour){
+            $("#3p").addClass("present")
+        }
+        else if (3 > currentHour){
+            $("#3p").addClass("future")
+        } 
+
+        if (4 < currentHour){
+            $("#4p").addClass("past")
+        }
+        else if (4 == currentHour){
+            $("#4p").addClass("present")
+        }
+        else if (4 > currentHour){
+            $("#4p").addClass("future")
+        }
+
+        if (5 < currentHour){
+            $("#5p").addClass("past")
+        }
+        else if (5 == currentHour){
+            $("#5p").addClass("present")
+        }
+        else if (5 > currentHour){
+            $("#5p").addClass("future")
+        }
 
 // var = 9
 // Element ID ="#hour-" =i;
